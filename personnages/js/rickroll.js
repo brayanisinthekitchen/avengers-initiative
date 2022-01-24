b1e776c215d3761669e34ea6442e8aea193f2044
REMI ROCHET <rereroro034@gmail.com>
2022-01-24 16:36:06 +0100

    ajout du rickroll

---
+++ personnages/js/rickroll.js
@@ -0,0 +1,11 @@
+
+var rickroll = new Audio('rickroll.mp3');
+
+var pressed = ""
+document.addEventListener("keypress", function(event){
+   console.log(event.key);
+   pressed += event.key;
+   if(pressed.includes("rickroll")){
+       rickroll.play()
+   }
+});
